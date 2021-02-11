e913bb946bb118e560c57e6f4f1bea413c476b76
Nik Medvedev <MedvedevKlaus@yandex.ru>
2021-02-11 18:24:22 +0300

    feat: create scroll to anchor and animation

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import toggleModal from './modules/modal';
 import sendForm from './modules/sendForm';
 import maskPhone from './modules/maskPhone';
 import falidation from './modules/falidation';
+import scroll from './modules/scroll';
 
 
+scroll();
 
 toggleModal();
 
---
+++ src/modules/scroll.js
@@ -0,0 +1,54 @@
+const scroll = () => {
+  const anchors = document.querySelectorAll('.anchor');
+
+  console.log(anchors);
+
+  for (const anchor of anchors) {
+    anchor.addEventListener('click', event => {
+      event.preventDefault();
+      const blockID = anchor.getAttribute('href').substr(1);
+      document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    });
+  }
+
+  const animateScroll = event => {
+
+    const target = event.target.closest('a[href*="#"]'),
+      speed = 1000;
+
+
+    if (target) {
+      const pageY = window.pageYOffset,
+        hash = target.href.replace(/[^#]*(.*)/, '$1'),
+        distTopPosition = document.querySelector(hash).getBoundingClientRect().top;
+
+      let start = 0;
+
+
+      const step = time => {
+        if (!start) start = time;
+
+        const progress = time - start;
+
+        const r = (distTopPosition < 0 ?
+          Math.max(pageY - progress / speed, pageY + distTopPosition) :
+          Math.min(pageY + progress / speed, pageY + distTopPosition));
+
+        window.scrollTo(0, r);
+
+        if (r < pageY + distTopPosition) requestAnimationFrame(step);
+      };
+
+      requestAnimationFrame(step);
+
+    }
+  };
+
+  document.querySelector('.anchor').addEventListener('click', animateScroll);
+}
+
+
+export default scroll;
